22c830e5f37d74f3790db1ead900ae713c3d0479
Erbil <erbil.silik@yandex.com>
2018-08-29 22:24:47 +0300

    Solve CORS problem for dev env

--- src/app/token.interceptor.ts
+++ src/app/token.interceptor.ts
@@ -1,13 +1,15 @@
-import { AuthService } from "./auth.service";
-import { Injectable } from "@angular/core";
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor
-} from "@angular/common/http";
-import { Observable } from "rxjs";
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Injectable()
+
 export class TokenInterceptor implements HttpInterceptor {
   constructor(public auth: AuthService) {}
   intercept(
